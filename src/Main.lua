2d5e9159dd68639a52741c039768098ba0f2f799
M00R3D <jobmurdan@hotmail.com>
2023-12-25 13:15:52 -0700

    2

--- src/Main.lua
+++ src/Main.lua
@@ -1,1 +1,31 @@
-print("helloworld")
+
+
+local x = 1
+local name ="job"
+x=x+1
+print(x)
+x=x+1
+print(x)
+x=x+1
+print(x)
+x=x+1
+print(x)
+
+print("helloworld "..name..".")
+
+local tabl = [[tres
+    uno
+    dos
+]]
+print(tabl)
+
+local bol=false
+bol=true
+
+Y=2
+local y =20
+print(y)
+print(Y)
+print(
+    type(bol)
+)
